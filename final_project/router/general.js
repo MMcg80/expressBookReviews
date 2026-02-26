b6c3dba18df07a95deb50cdb95fc178f36c52e74
MMcg80 <shirokuma025@gmail.com>
2026-02-26 09:34:36 -0500

    GET by ISBN/Author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,20 +13,40 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    const all_isbns = Object.keys(books); // Get [ "1", "2", "3", ... ]
+    let booksByAuthor = [];
+  
+    all_isbns.forEach(isbn => {
+      if (books[isbn].author === author) {
+        booksByAuthor.push({
+          isbn: isbn,
+          title: books[isbn].title,
+          reviews: books[isbn].reviews
+        });
+      }
+    });
+  
+    if (booksByAuthor.length > 0) {
+      return res.status(200).json(booksByAuthor);
+    } else {
+      return res.status(404).json({ message: "No books found by this author" });
+    }
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
